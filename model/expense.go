706de89df5e5172f3cb7876652437f80acf84b84
sourabhbhagat <sbhagat@tekion.com>
2017-04-02 09:14:02 +0530

    initial commit

---
+++ model/expense.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+//Expense - data model
+type Expense struct {
+	ID          string    `bson:"_id" json:"id"`
+	ExpenseTime time.Time `bson:"expenseTime" json:"expenseTime"`
+	IsLunch     bool      `bson:"isLunch" json:"isLunch"`
+	Total       float32   `bson:"total" json:"total"`
+}
